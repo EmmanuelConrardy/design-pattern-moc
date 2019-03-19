d2983bc52b4ea61fc532f68e7da585bc80e25b38
Manu <emmanuel.conrardy@arolla.fr>
2019-03-19 19:18:19 +0100

    TripKataService

---
+++ TripServicesKata.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TripServicesKata.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TripServicesKata.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a203da6-5ad1-4232-ba7a-195391e199ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TripServicesKata.Test/TripServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripServiceKata.Trip;
+using TripServiceKata.User;
+
+namespace TripServicesKata.Test
+{
+
+    [TestClass]
+    public class TripServiceTest
+    {
+        public const User UserAnonyme = null;
+
+        [TestMethod]
+        public void When_User_Is_Anonyme_Should_Return_No_Trip()
+        {
+            var tripService = new TripService();
+            
+            var trips = tripService.GetTripsByUser(UserAnonyme);
+
+            Assert.AreEqual(0, trips.Count);
+        }
+    }
+}
