25a6107fd345036551238f079ec4107f821d039c
Manu <emmanuel.conrardy@arolla.fr>
2019-05-14 10:05:02 +0200

    Init MarvelKata

---
+++ MarvelKataDesign/JusticeLeague.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JusticeLeague.Test
+{
+    public class JusticeLeagueBuilderTest
+    {
+        [Fact]
+        public void Justice_Characters()
+        {
+            //GET /v1/public/characters
+        }
+    }
+}
